585e90394043f5dbc790673b55c1337aba384602
Nocky <cocytus1223@gmail.com>
2018-12-24 00:33:36 +0800

    searchbar

--- components/public/header/index.vue
+++ components/public/header/index.vue
@@ -1,24 +1,29 @@
 <template>
-  <el-row
-    :gutter="15"
-    class="m-header">
-    <el-col :span="6"><geo/></el-col>
-    <el-col :span="6"><user/></el-col>
-    <el-col :span="6"><div class="grid-content bg-purple"/></el-col>
-  </el-row>
+  <div class="m-header">
+    <el-row>
+      <el-col>
+        <top-bar/>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col>
+        <search-bar/>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
-import Geo from './geo.vue'
-import User from './user.vue'
+import topBar from './topbar.vue'
+import searchBar from './searchbar.vue'
 export default {
   components: {
-    Geo,
-    User
+    topBar,
+    searchBar
   }
 }
 </script>
 
 <style lang="scss">
-@import '@/assets/css/public/layout.scss';
+@import '@/assets/css/public/header/index.scss';
 </style>
---
+++ components/public/header/topbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-row
+    :gutter="0"
+    class="m-header">
+    <el-col :span="5"><geo/></el-col>
+    <el-col :span="5"><user/></el-col>
+    <el-col :span="14">
+      <Navbar/>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import Geo from './geo.vue'
+import User from './user.vue'
+import Navbar from './nav.vue'
+export default {
+  components: {
+    Geo,
+    User,
+    Navbar
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/css/public/header/index.scss';
+</style>
